c1add6ae2a1b2958a5bbeb31a019efcadc926f37
sleepingcat <sleepingcat@lonely-desk.top>
2024-05-10 12:17:22 +0100

    chore: vars

--- custom_aliases.zsh
+++ custom_aliases.zsh
@@ -1,7 +1,7 @@
 alias 1f="onefetch --no-art --no-color-palette --disabled-fields churn authors --true-color never"
 alias nvidia-settings='nvidia-settings --config="$XDG_CONFIG_HOME"/nvidia/settings '
 alias zt='time zsh -i -c exit'
-alias ts='tailscale status'
+alias ns='netbird status'
 alias ka='killall -q'
 alias w='_weather() { curl -s wttr.in/“${1:-bristol}” | head -n 7 ;}; _weather'
 alias wget='wget --hsts-file="$XDG_CACHE_HOME/wget/history"'
